8d2452fd94e63e8aa024d86fb060dcc6094d1453
shchae7 <seunghyunchae@chaeseunghyeons-MacBook-Pro.local>
2020-05-07 16:38:48 +0900

    4_1mux complete

--- 4_1mux.v
+++ 4_1mux.v
@@ -10,6 +10,8 @@ module MUX4_1 (i1, i2, i3, i4, s, result);
 
     output [`WORD_SIZE - 1 : 0] result;
 
+    wire [`WORD_SIZE - 1:0] result;
+
     assign result = s[1] ? (s[0] ? i4 : i3) : (s[0] ? i2 : i1); 
 
 endmodule
