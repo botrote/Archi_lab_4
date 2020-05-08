4afb691f2f137d39b9802bc2d2d6a280213c447d
shchae7 <seunghyunchae@chaeseunghyeons-MacBook-Pro.local>
2020-05-09 00:10:36 +0900

    alu display added

--- ALU.v
+++ ALU.v
@@ -30,31 +30,69 @@ module alu(
         `FUNC_ADD : 
 		begin
 			assign aResult = op1 + op2;
-			$display("op1: %d + op2: %d = aResult: %d", op1, op2, aResult);
+			$display("%d + %d = %d", op1, op2, aResult);
 		end
         `FUNC_SUB : 
 		begin
 			assign aResult = op1 - op2;
-			$display("op1: %d - op2: %d = aResult: %d", op1, op2, aResult);
+			$display("%d - %d = %d", op1, op2, aResult);
 		end
         `FUNC_AND : 
 		begin
 			assign aResult = op1 & op2;
+            $display("%d & %d = %d", op1, op2, aResult);
 		end
-        `FUNC_ORR : assign aResult = op1 | op2;
-        `FUNC_NOT : assign aResult = ~op1;
-        `FUNC_TCP : assign aResult = ~op1 + 1;
-        `FUNC_SHL : assign aResult = op1 << 1;
-        `FUNC_SHR : assign aResult = ($signed(op1) >>> 1);	
-        `FUNC_BNE : assign bResult = ((op1 == op2) ? 0 : 1);
-        `FUNC_BEQ : assign bResult = ((op1 == op2) ? 1 : 0);
-        `FUNC_BGZ : assign bResult = ((op1 > 0) ? 1 : 0);
-        `FUNC_BLZ : assign bResult = ((op1 < 0) ? 1 : 0);
+        `FUNC_ORR :
+            begin
+                assign aResult = op1 | op2;
+                $display("%d | %d = %d", op1, op2, aResult);
+            end
+        `FUNC_NOT : 
+            begin
+                assign aResult = ~op1;
+                $display("~%d = %d", op1, aResult);
+            end
+        `FUNC_TCP : 
+            begin
+                assign aResult = ~op1 + 1;
+                $display("~%d + 1 = %d", op1, aResult);
+            end
+        `FUNC_SHL : 
+            begin
+                assign aResult = op1 << 1;
+                $display("%d << 1 = %d", op1, aResult);
+            end
+        `FUNC_SHR : 
+            begin
+                assign aResult = ($signed(op1) >>> 1);	
+                $display("%d >> 1 = %d", op1, aResult);
+            end
+        `FUNC_BNE : 
+            begin
+                assign bResult = ((op1 == op2) ? 0 : 1);
+                $display("%d %d => %d", op1, op2, bResult);
+            end
+        `FUNC_BEQ : 
+            begin
+                assign bResult = ((op1 == op2) ? 1 : 0);
+                $display("%d %d => %d", op1, op2, bResult);
+            end
+        `FUNC_BGZ : 
+            begin
+                assign bResult = ((op1 > 0) ? 1 : 0);
+                $display("%d %d => %d", op1, op2, bResult);
+            end
+        `FUNC_BLZ : 
+            begin
+                assign bResult = ((op1 < 0) ? 1 : 0);
+                $display("%d %d => %d", op1, op2, bResult);
+            end
 
 
         `FUNC_LHI:
             begin
                 assign aResult = op2 << 8;
+                $display("%d << 8 = %d", op2, aResult);
             end
         endcase
     end
